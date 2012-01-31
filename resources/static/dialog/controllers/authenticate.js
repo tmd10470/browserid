9d46b4fed7fc49719d93a20bf04b24c64dfb5679
Shane Tomlinson <stomlinson@mozilla.com>
2012-01-31 18:11:49 +0000

    Clean up new user flow!
    * Instead of showing a "set password" button, go straight to enter password screen.
    * Skip the showing of the email address and go straight to setting the user up in the verify_email_address page.
    
    issue #1000

--- resources/static/dialog/controllers/authenticate.js
+++ resources/static/dialog/controllers/authenticate.js
@@ -57,11 +57,13 @@ BrowserID.Modules.Authenticate = (function() {
       if(info.type === "primary") {
         self.close("primary_user", info, info);
       }
-      else if(info.known) {
-        enterPasswordState.call(self);
-      } else {
-        self.close("new_user", {email: email});
-        //createSecondaryUserState.call(self);
+      else {
+        if(info.known) {
+          enterPasswordState.call(self);
+        }
+        else {
+          createSecondaryUser.call(self);
+        }
       }
     }
   }
